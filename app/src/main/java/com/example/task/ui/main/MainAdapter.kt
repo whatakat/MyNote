cc764a11ca3e875526539ea3d2b96d3e81f970af
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-09 18:24:49 +0300

    add MainAdapter

---
+++ app/src/main/java/com/example/task/ui/main/MainAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.task.ui.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.task.R
+import com.example.task.data.model.Note
+
+class MainAdapter: RecyclerView.Adapter<NoteViewHolder>() {
+    var notes:List<Note> = listOf()
+    set(value){
+        field = value
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view  = inflater.inflate(R.layout.activity_main,parent, false) //item_note?
+        return NoteViewHolder(view)
+    }
+
+    override fun getItemCount() = notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) : Unit {
+        holder.bind(notes[position])
+    }
+}
