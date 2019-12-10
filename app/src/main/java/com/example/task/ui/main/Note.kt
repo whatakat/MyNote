6a5ff3200f2fa616f70cd24cd4aed0ebd8045236
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-12-10 15:01:32 +0300

    data class Note

---
+++ app/src/main/java/com/example/task/ui/main/Note.kt
@@ -0,0 +1,29 @@
+package com.example.task.ui.main
+
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Parcelize
+data class Note(val id:String,
+                val title:String,
+                val note: String,
+                val color: Color = Color.WHITE,
+                val lastChanged: Date = Date()): Parcelable {
+    override fun equals(other: Any?): Boolean {
+        if(this==other)return true
+       // if(JavaClass !=other?.JavaClass) return false
+
+        other as Note
+
+        if (id !=other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+}
