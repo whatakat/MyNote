ce0e18762894a7f157d136b8c8322e414290a253
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-22 22:58:48 +0300

    add NotesRepository

---
+++ app/src/main/java/com/example/task/data/NotesRepository.kt
@@ -0,0 +1,12 @@
+package com.example.task.data
+
+import com.example.task.data.entity.Note
+import com.example.task.data.provider.RemoteDataProvider
+
+class NotesRepository(private val remoteProvider: RemoteDataProvider) {
+    fun getNotes() = remoteProvider.subscribeToAllNotes()
+    suspend fun saveNote(note: Note) = remoteProvider.saveNote(note)
+    suspend fun getNoteById(id: String) = remoteProvider.getNoteById(id)
+    suspend fun getCurrentUser() = remoteProvider.getCurrentUser()
+    suspend fun deleteNote(noteId: String) = remoteProvider.deleteNote(noteId)
+}
