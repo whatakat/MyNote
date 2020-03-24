4fdb364307a7a328842cb8f50aa5569760e2d5c1
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-24 13:20:17 +0300

    add SplashViewState

---
+++ app/src/main/java/com/example/task/ui/splash/SplashViewState.kt
@@ -0,0 +1,5 @@
+package com.example.task.ui.splash
+
+import com.example.task.ui.base.BaseViewState
+
+class SplashViewState(authenticated: Boolean? = null, error: Throwable? = null) : BaseViewState<Boolean?>(authenticated, error)
