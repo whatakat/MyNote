f7feab6a24fc57375ed120a0a5392b6ddf50b8cd
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-24 13:33:22 +0300

    add import

--- app/src/main/java/com/example/task/ui/App.kt
+++ app/src/main/java/com/example/task/ui/App.kt
@@ -1,6 +1,10 @@
 package com.example.task.ui
 
 import android.app.Application
+import com.example.task.di.appModule
+import com.example.task.di.mainModule
+import com.example.task.di.noteModule
+import com.example.task.di.splashModule
 import org.koin.android.ext.android.startKoin
 
 class App: Application() {
