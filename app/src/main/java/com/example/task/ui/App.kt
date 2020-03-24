fa26747b42a13ede1d263883b3771918c1a30d90
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-24 13:27:01 +0300

    add App & implementation coin

---
+++ app/src/main/java/com/example/task/ui/App.kt
@@ -0,0 +1,12 @@
+package com.example.task.ui
+
+import android.app.Application
+import org.koin.android.ext.android.startKoin
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(this, listOf(appModule, splashModule, mainModule, noteModule))
+    }
+}
